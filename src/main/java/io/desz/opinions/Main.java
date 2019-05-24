8d1702bde09b3f1ce9ed9aa4d3cffc5acab1c0fe
Denis <dev.szczukocki.denis@gmail.com>
2019-05-24 17:24:46 +0200

    chg desc and remove guice

--- src/main/java/io/desz/opinions/Main.java
+++ src/main/java/io/desz/opinions/Main.java
@@ -23,7 +23,7 @@ public class Main {
         Opinion opinion = new OpinionBuilder()
                 .setReviewer(denis)
                 .setThing(new Thing("wallet from TheWallet company", Thing.Type.PRODUCT))
-                .setDescription(new Description("don't buy lego", "this item sucks"))
+                .setDescription(new Description("don't buy it", "the wallet's quality is really poor"))
                 .setRate(Rate.NOT_RECOMMEND)
                 .setReviewDateTime(LocalDateTime.of(2010, Month.JANUARY, 10, 10, 15))
                 .createOpinion();
